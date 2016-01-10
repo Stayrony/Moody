911d0a7375ef84a33f4520573d3f53e6ebe9f375
Stayrony <beyoncem7@gmail.com>
2016-01-10 02:35:46 -0800

    add IUserDalManager

---
+++ Moody.DAL.Contract/IUserDalManager.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IUserDalManager.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The UserDalManager interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Moody.DAL.Contract
+{
+    using Moody.Service.Domain;
+
+    /// <summary>
+    ///     The UserDalManager interface.
+    /// </summary>
+    public interface IUserDalManager
+    {
+        /// <summary>
+        /// The get user on login.
+        /// </summary>
+        /// <param name="login">
+        /// The login.
+        /// </param>
+        /// <returns>
+        /// The <see cref="User"/>.
+        /// </returns>
+        User GetUserOnLogin(string login);
+
+        User AddNewUser(User newUser);
+
+        void DeleteUser(User user);
+    }
+}
