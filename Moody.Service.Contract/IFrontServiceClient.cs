d7e8f2e56ef638bd0c9642b378fd858c372ee9fe
Stayrony <beyoncem7@gmail.com>
2016-01-16 00:52:20 -0800

    add Service.Contract with IFrontServiceClient

---
+++ Moody.Service.Contract/IFrontServiceClient.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IFrontServiceClient.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The FrontServiceClient interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Moody.Service.Contract
+{
+    using Moody.Service.Domain;
+
+    /// <summary>
+    ///     The FrontServiceClient interface.
+    /// </summary>
+    public interface IFrontServiceClient
+    {
+        /// <summary>
+        /// The enter the system.
+        /// </summary>
+        /// <param name="loginInfo">
+        /// The login info.
+        /// </param>
+        /// <returns>
+        /// The <see cref="User"/>.
+        /// </returns>
+        User EnterTheSystem(LoginInfo loginInfo);
+    }
+}
