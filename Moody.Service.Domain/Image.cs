b5af40a567080ac5c3a027dc9f4ca1c307f2c977
Stayrony <beyoncem7@gmail.com>
2016-01-23 17:40:34 +0200

    add Image class

---
+++ Moody.Service.Domain/Image.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Image.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The image.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Moody.Service.Domain
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    ///     The image.
+    /// </summary>
+    public class Image
+    {
+        /// <summary>
+        ///     Gets or sets the image path.
+        /// </summary>
+        public string ImagePath { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the time created.
+        /// </summary>
+        public DateTime TimeCreated { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the tags.
+        /// </summary>
+        public List<string> Tags { get; set; }
+    }
+}
