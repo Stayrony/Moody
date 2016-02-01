92b5f625482cc60cec605f05b1955e91165956d5
Stayrony <beyoncem7@gmail.com>
2016-02-01 22:45:46 +0200

    add Tag class

---
+++ Moody.Service.Domain/Tag.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Tag.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The tag.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Moody.Service.Domain
+{
+    /// <summary>
+    ///     The tag.
+    /// </summary>
+    public class Tag
+    {
+        /// <summary>
+        ///     Gets or sets the tag id.
+        /// </summary>
+        public int TagId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
